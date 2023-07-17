8144bc3c6c90afd9a205c7d0fa8f5d325f8251f1
Anujraghuwanshi2408 <128280445+Anujraghuwanshi2408@users.noreply.github.com>
2023-07-18 01:32:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // complete this js code
+function Person (name ,age){
+	this.name = name;
+	this.age = age;
+}
 Person.prototype.greet = () =>{
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`)
 } 
